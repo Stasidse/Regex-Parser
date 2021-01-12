0f083d65699136fe1ea1d0a12e31adfc7cc5a3be
Stasidse <64648835+Stasidse@users.noreply.github.com>
2021-01-13 00:03:26 +0100

    Add files via upload

---
+++ Regex-Parser/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Regex_Parser
+{
+    class Program
+    {
+        public static string Parsing(string from, string one, string two)
+        {
+            Match val = Regex.Match(from, "(?<=" + one + ")(.*?)(?=" + two + ")");
+            return val.ToString();
+        }
+    }
+}
